8f4e0e3f8ca6152d7893a4778c6de515b14f0aa1
Joaquin <jiaraya4@uc.cl>
2021-05-03 06:53:24 -0400

    removed useless controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,7 @@
-export * from './ping.controller';
-export * from './artist-album.controller';
 export * from './album-track.controller';
-export * from './track-album.controller';
-export * from './album-artist.controller';
-export * from './artist.controller';
 export * from './album.controller';
+export * from './artist-album.controller';
+export * from './artist.controller';
+export * from './ping.controller';
 export * from './track.controller';
+
